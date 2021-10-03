b74560a12219cf8fa520a1e8ebcbb05c3687071c
marinabardier <balatcaiam@gmail.com>
2021-10-03 15:38:12 -0400

    Car in MAIN Branch

---
+++ src/setup/Car.java
@@ -0,0 +1,13 @@
+package setup;
+
+public class Car {
+
+    // this car is in Master Branch
+
+    // the owner is James
+
+    // will we have conflict?
+
+    // we will have conflict,
+    // cause it is different text in different branches for car
+}
