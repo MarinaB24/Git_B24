0e2a0b5bea39d2501c0bfd9df2f1e820c27e2e09
marinabardier <balatcaiam@gmail.com>
2021-10-03 16:20:22 -0400

    Car in MAIN Branch

--- src/setup/Car.java
+++ src/setup/Car.java
@@ -2,7 +2,7 @@ package setup;
 
 public class Car {
 
-<<<<<<< HEAD
+
     // this car is in Master Branch
 
     // the owner is James
@@ -11,9 +11,9 @@ public class Car {
 
     // we will have conflict,
     // cause it is different text in different branches for car
-=======
-    // we have new car
+
+    // we have a new car
 
     // he owner is James Bond
->>>>>>> James
+
 }
